a652580aed285ad881fc97a99455c982d6f23acb
maxime guirado <maxime guirado>
2016-04-28 13:18:31 +0200

    la base

---
+++ SiteGame/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SiteGame.Startup))]
+namespace SiteGame
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
